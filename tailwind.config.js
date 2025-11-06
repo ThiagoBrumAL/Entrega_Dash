887c5237dc76042aa120a6b51ea0edc9a7854aa9
ggbiel03 <gabrielsilva03@gmail.com>
2025-11-06 20:15:16 -0300

    Att Graficos

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2692f0',
+      },
+    },
+  },
+  content: ["./*.html", "./js/*.js"],
+};
+
+
